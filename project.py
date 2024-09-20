e0152c13394bf2f7979880abbf4d48216f58ded8
nidianlasso <nidianlassocha@gmail.com>
2024-09-19 22:07:31 -0500

    Validacion credenciales de usuario

--- project.py
+++ project.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 import datetime
-from query import obtenerUsuarios
+from query import obtenerUsuarios, check_credentials, validarLogin
 
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, session
 app = Flask(__name__, static_folder='static', template_folder='template')
 
-@app.route('/login')
+@app.route('/login', methods = ['POST', 'GET'])
 def login():     
-    obtenerUsuarios()
+    error = None
+    if request.method == "POST":
+        identificacion = request.form['identificacion']
+        contrasena = request.form['contrasena']
+        print(identificacion, contrasena)
+        #LLAMADO A LA FUNCION VALIDARLOGIN
+        return validarLogin(identificacion, contrasena)
+    check_credentials(0,0)
     return render_template('login.html')
+
+#LLAMADO AL TEMPLATE ADMINISTRADOR
+@app.route('/administrator')
+def administrator():
+    return render_template('Administrator/administrator.html')
+
+#LLAMADO AL TEMPLATE MIEMBRO
+@app.route('/profile-member')
+def profile_member():
+    return render_template('member/profile.html')
+
 
 if __name__ =='__main__':
     app.run(port =4000, debug =True)
--- query.py
+++ query.py
@@ -11,4 +11,30 @@ cursor = connection.cursor()
 def obtenerUsuarios():
     cursor.execute("SELECT * FROM bd_gimnasio2.usuario")
     rows = cursor.fetchall()
+    cursor.close()  # Asegúrate de cerrar el cursor
     return rows
+
+def validarLogin(identificacion, contrasena):
+    info_user = check_credentials(identificacion, contrasena)
+    if info_user:
+        rol = info_user[0]
+        resp = make_response()
+        resp.set_cookie('identificacion', identificacion)
+
+        if rol =="Administrador":
+            return make_response(redirect(url_for('administrator')))
+        elif rol =="Miembro":
+            return make_response(redirect(url_for('profile_member')))
+    
+    error = "Credenciales incorrectas. Intente de nuevo."
+    return render_template('login.html', error=error)
+
+
+#---VERIFICAR LAS CREDENCIALES DE LOS USUARIOS
+def check_credentials(identificacion, contrasena):
+    cursor.execute("SELECT r.nombre FROM bd_gimnasio2.usuario u INNER JOIN rol r ON u.id_rol = r.id_rol WHERE identificacion = %s AND contrasena = %s", (identificacion, contrasena))
+    info_user = cursor.fetchone()
+    print("ESTA ES LA CONSULTA DEL MIEMBRO")
+    print(info_user)
+    return info_user
+#---VERIFICAR LAS CREDENCIALES DE LOS USUARIOS
